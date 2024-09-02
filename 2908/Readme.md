613541e4018a02d45aca85ef4f1c65449d9df995
RajatSharma2004 <rajatsharmayo4@gmail.com>
2024-09-02 13:28:57 +0530

    Second commit

---
+++ 2908/Readme.md
@@ -0,0 +1,4 @@
+29-08-2024
+1. Find the data where students have age = 22 and marks=19
+2. Find the students who has marks more the 80 and age more than 18
+3. Find students whose grades are A++
---
+++ 2908/code.js
@@ -0,0 +1,36 @@
+const students = [
+  {
+  name: "John",
+  age: 22,
+  marks: 50,
+  subject: "Math"
+  },
+  {
+    name: "Emily",
+    age: 21,
+    marks: 48,
+    subject: "Science"
+  },
+  {
+    name: "Michael",
+    age: 20,
+    marks: 42,
+    subject: "History" 
+  },
+  {
+    name: "Sarah",
+  age: 23,
+  marks: 56,
+  subject: "Computer Science"},
+  {
+    name: "David",
+    age: 24,
+    marks: 52,
+    subject: "Maths"
+    },
+  {name: "Ram",
+    age: 25,
+    marks: 58,
+    subject: "English"}
+  
+]
